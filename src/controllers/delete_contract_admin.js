427ebb7a296f890e5db279a57556b2faad0ec602
dvconsultores <adominguez@dvconsultores.com>
2024-05-13 21:11:07 -0400

    chore: Refactor delete_contract_admin.js for improved readability and performance

--- src/controllers/delete_contract_admin.js
+++ src/controllers/delete_contract_admin.js
@@ -15,7 +15,7 @@ const InMemoryCache = require('apollo-cache-inmemory').InMemoryCache
 
 const client = new ApolloClient({
   link: createHttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/hrpalencia/p2pmainnet',
+    uri: 'https://api.studio.thegraph.com/proxy/74187/p2pmainnet/version/latest',
     fetch
   }),
   cache: new InMemoryCache()
@@ -42,82 +42,115 @@ const fetch_users = async () => {
   }
 }
 
+const fetch_orders = async () => {
+  const query = gql`
+    query MyQuery {
+        ordersells {
+          signer_id
+          order_id
+        }
+    }
+  `
+  let users;
+  try {
+    const result = await client.query({
+      query
+    })
+    users = result.data.ordersells
+  } catch (error) {
+    console.log(error)
+    users = []
+  }
+  return users;
+}
+
+// Near connection constants
+const VLCONTRACTNAME = 'v4.nearp2pdex.near'
+const VLSIGNER_ID = 'adminp2p.near'
+const VLSIGNER_PRIVATEKEY = 'ed25519:vZW5NpAFgVca381zcq3tTWoq8dbxuB5WkumDNVS6GS3zDJpgrrRxFYgv9RLqc1YbawqEDdxXJQXDnTs3CYor72n'
+const VLNETWORK = 'mainnet'
+
+const keyStore = new keyStores.InMemoryKeyStore()
+const config = CONFIG(keyStore, VLNETWORK)
+const keyPair = KeyPair.fromString(VLSIGNER_PRIVATEKEY)
+keyStore.setKey(VLNETWORK, VLSIGNER_ID, keyPair)
+const near = new Near(config)
+const account = new Account(near.connection, VLSIGNER_ID)
+
+// Contract call buy or sell
+const contract = new Contract(account, VLCONTRACTNAME, {
+  viewMethods: ['get_subcontract', 'get_subcontract_type', 'get_order_sell'],
+  changeMethods: ['delete_contract_admin', 'delete_user_contract_list'],
+  sender: account
+})
+
 // Callback autodispute
 async function callbackFunction () {
-  // Near conection
-  // eslint-disable-next-line no-undef
-  const VLCONTRACTNAME = 'v4.nearp2pdex.near'
-  const VLSIGNER_ID = 'adminp2p.near'
-  const VLSIGNER_PRIVATEKEY = 'ed25519:vZW5NpAFgVca381zcq3tTWoq8dbxuB5WkumDNVS6GS3zDJpgrrRxFYgv9RLqc1YbawqEDdxXJQXDnTs3CYor72n'
-  const VLNETWORK = 'mainnet'
-
-  const keyStore = new keyStores.InMemoryKeyStore()
-  // Connect to network
-  // eslint-disable-next-line no-use-before-define
-  const config = CONFIG(keyStore, VLNETWORK)
-  const CONTRACT_NAME = VLCONTRACTNAME
-  const SIGNER_ID = VLSIGNER_ID
-  const SIGNER_PRIVATEKEY = VLSIGNER_PRIVATEKEY
-  const NETWORK = VLNETWORK
-
-  const keyPair = KeyPair.fromString(SIGNER_PRIVATEKEY)
-  keyStore.setKey(NETWORK, SIGNER_ID, keyPair)
-  const near = new Near(config)
-  const account = new Account(near.connection, SIGNER_ID)
-  // eslint-disable-next-line no-unused-vars
-  const response = null
-  const data = null
-  const datatype = null
-  const orderssell = null
-
-  // Contract call buy or sell
-  const contract = new Contract(account, CONTRACT_NAME, {
-    viewMethods: ['get_subcontract', 'get_subcontract_type', 'get_order_sell'],
-    changeMethods: ['delete_contract_admin', 'delete_user_contract_list'],
-    sender: account
-  })
+  const orderssell = await fetch_orders();
+  console.log('First Call')
   try {
-    // await contract.delete_user_contract_list({ user_id: 'andresdom.near' })
-    // Get the users data
-    const query = gql`
-    query MyQuery {
-        datausers {
-          user_id
+    await fetch_users()
+
+    // Loop and get subcontract
+    const promises = users.map(async (user) => {
+      const data = await contract.get_subcontract_type({ user_id: user.user_id })
+      // If the user have subcontract, get the type
+      if (data === 2) {
+        const filteredOrders = orderssell.filter(order => order && order.signer_id === item.walletname);
+        const length = filteredOrders.length;
+        if (length === 0) { // Validating open order's
+          console.log('Deleting', user.user_id + ' subcontract')
+          await contract.delete_contract_admin({
+            callbackUrl: '', // callbackUrl after the transaction approved (optional)
+            meta: 'some info', // meta info (optional)
+            args: {
+              user_id: user.user_id
+            },
+            gas: '300000000000000' // attached GAS (optional)
+          })
         }
       }
-    `
-    try {
-      const result = await client.query({
-        query
-      })
-      users = result.data.datausers
-      // console.log(users)
-      // Loop and get subcontract
-      for (let i = 0; i < users.length; i++) {
-        // console.log(users[i].user_id)
-        this.data = await contract.get_subcontract_type({ user_id: users[i].user_id })
-        // If the user have subcontract, get the type
-        if (this.data === 2) {
-          this.orderssell = await contract.get_order_sell({ signer_id: users[i].user_id })
-          if (this.orderssell.total_index === 0) { // Validating open order's
-            console.log('Deleting', users[i].user_id + ' subcontract')
-            await contract.delete_contract_admin({
-              callbackUrl: '', // callbackUrl after the transaction approved (optional)
-              meta: 'some info', // meta info (optional)
-              args: {
-                user_id: users[i].user_id
-              },
-              gas: '300000000000000' // attached GAS (optional)
-            })
-          }
-        }
-      }
-    } catch (error) {
-      console.log(error)
-      users = []
-    }
+    })
+
+    await Promise.all(promises)
   } catch (err) {
     console.log(err)
+  }
+  console.log('Second Call')
+  try {
+    const response = await fetch('https://nearp2p.com/wallet-p2p/api/v1/wallet/verify-all-wallets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    
+    
+    const promises = data.data.map(async (item) => {
+      const subcontractType = await contract.get_subcontract_type({ user_id: item.walletname });
+      if (subcontractType === 2) {
+        const filteredOrders = orderssell.filter(order => order && order.signer_id === item.walletname);
+        const length = filteredOrders.length;
+        if (length === 0) {
+          console.log('Deleting', item.walletname + ' subcontract');
+          // Uncomment the following line to actually delete the contract
+          await contract.delete_contract_admin({
+            callbackUrl: '', // callbackUrl after the transaction approved (optional)
+            meta: 'some info', // meta info (optional)
+            args: {
+              user_id: item.walletname
+            },
+            gas: '300000000000000' // attached GAS (optional)
+          }); // <-- Add the missing closing curly brace here
+        }
+      }
+    });
+    
+    await Promise.all(promises);
+
+  } catch (error) {
+    console.log(error)
   }
 }
 
